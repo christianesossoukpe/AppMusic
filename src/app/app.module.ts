e7cea38dbf396b946474d3ff9d95537b755f5198
Christiane Sossoukpe <christdebo68@gmail.com>
2024-08-12 10:22:40 +0100

    projet app-music ChristDeby.initialisation,form ulaires et nav.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FormComponent } from './form/form.component';
+import { FormInscriptionComponent } from './form-inscription/form-inscription.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormComponent,
+    FormInscriptionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/form-inscription/form-inscription.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-inscription',
+  templateUrl: './form-inscription.component.html',
+  styleUrl: './form-inscription.component.css'
+})
+export class FormInscriptionComponent {
+
+}
